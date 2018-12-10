ff33c0a04b81008b5b0f83cb8a3af83a743923bc
Rishabh Chawla <rishabh31121999@gmail.com>
2018-12-10 16:59:25 +0530

    feat(gitignore): remove dist from gitignore

---
+++ front/dist/precache-manifest.fe9f39335519156a4c36390d776b9c7b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfc450967f87380bb92b",
+    "url": "/css/about.5a77df5e.css"
+  },
+  {
+    "revision": "bfc450967f87380bb92b",
+    "url": "/js/about.2887eb9f.js"
+  },
+  {
+    "revision": "5a54d2dc9fc31c4815b1",
+    "url": "/css/app.28f02204.css"
+  },
+  {
+    "revision": "5a54d2dc9fc31c4815b1",
+    "url": "/js/app.c1e4ed16.js"
+  },
+  {
+    "revision": "4d37c784b347d71ddebb",
+    "url": "/css/chunk-vendors.d8f7b2ab.css"
+  },
+  {
+    "revision": "4d37c784b347d71ddebb",
+    "url": "/js/chunk-vendors.1172c991.js"
+  },
+  {
+    "revision": "627b87ecf3181fe04a8d",
+    "url": "/css/search.1be67f6a.css"
+  },
+  {
+    "revision": "627b87ecf3181fe04a8d",
+    "url": "/js/search.115d9bf3.js"
+  },
+  {
+    "revision": "b5b62bca75852d7ad26a2f2712b55d53",
+    "url": "/img/variation_dist.b5b62bca.png"
+  },
+  {
+    "revision": "7a96a06454c91feb603284c18a203bc9",
+    "url": "/img/discovery_all.7a96a064.png"
+  },
+  {
+    "revision": "e26e995256ff0629c2767a64712134db",
+    "url": "/img/space.e26e9952.png"
+  },
+  {
+    "revision": "5105e479ba122fa5fbf2c2845c099f3f",
+    "url": "/img/discovery.5105e479.png"
+  },
+  {
+    "revision": "52d697f4688bd9f0e18068632d720c2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
